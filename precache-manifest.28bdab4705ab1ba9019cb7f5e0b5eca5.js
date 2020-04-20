2e30d9608a600546232a20aac339239cce6729e8
SOVEREIGN <isimemeni@gmail.com>
2020-04-20 16:22:13 -0400

    Updates

---
+++ precache-manifest.28bdab4705ab1ba9019cb7f5e0b5eca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c5e5fea76cc8a2416ad86f1b3aaf35",
+    "url": "/Weekndr/index.html"
+  },
+  {
+    "revision": "cf5b289d0a88bac1210f",
+    "url": "/Weekndr/static/css/main.84b7d46c.chunk.css"
+  },
+  {
+    "revision": "909b0088b7f114801c33",
+    "url": "/Weekndr/static/js/2.74e4f79d.chunk.js"
+  },
+  {
+    "revision": "17dffe90612e048fd9b4a769ac11b9cb",
+    "url": "/Weekndr/static/js/2.74e4f79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5b289d0a88bac1210f",
+    "url": "/Weekndr/static/js/main.ceec0d4b.chunk.js"
+  },
+  {
+    "revision": "637c1e1c207f26f890ac",
+    "url": "/Weekndr/static/js/runtime-main.f9075d0d.js"
+  }
+]);
