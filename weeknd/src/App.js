13a385a0976af4b42dcf9cadafe8f482a5116106
SOVEREIGN <isimemeni@gmail.com>
2020-04-20 16:20:21 -0400

    added gh pages

--- weeknd/src/App.js
+++ weeknd/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 import HomePage from './Pages/HomePage'
 import FullPage from './Pages/FullPage'
 
